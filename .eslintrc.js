daf9fed9d3f71bb83651bc37754212400857d7cb
fabianmolinab <molinafabian11b@gmail.com>
2022-04-28 03:30:53 -0500

    Agregado ESLINT + Husky

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+const RULES = {
+  OFF: 'off',
+  WARN: 'warn',
+  ERROR: 'error'
+}
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: ['plugin:react/recommended', 'standard'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/react-in-jsx-scope': RULES.OFF,
+    'react/prop-types': RULES.OFF,
+    'react/jsx-uses-react': RULES.ERROR,
+    'react/jsx-uses-vars': RULES.ERROR,
+    'no-console': RULES.WARN,
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: true,
+        ignoreDeclarationSort: true,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+        allowSeparatedGroups: false
+      }
+    ]
+  }
+}
